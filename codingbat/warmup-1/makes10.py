2f8d4d933546da8860469e1c4b35b92f7c8a3bfc
0x7070 <raykhelson.ielon@gmail.com>
2025-01-14 14:05:08 -0500

    commit makes10.py

---
+++ codingbat/warmup-1/makes10.py
@@ -0,0 +1,4 @@
+def makes10(a, b):
+  return ( (a == 10) or (b == 10) or ((a + b) == 10) )
+  # `return ()` evaluates and returns the results of the conditional statement.
+  # in this case, if any of the conditionals within the outer () are true, it will return True
