70f31cd8c03af6329cd6934b730a840b425ce884
0x7070 <raykhelson.ielon@gmail.com>
2025-01-14 13:55:15 -0500

    commit diff21.py

---
+++ codingbat/warmup-1/diff21.py
@@ -0,0 +1,9 @@
+def diff21(n):
+  difference = abs(n - 21)
+  if n > 21:
+    return 2 * difference
+  else:
+    return difference
+
+# `abs(n)` is part of Pythons standard library
+# https://www.w3schools.com/python/ref_func_abs.asp
