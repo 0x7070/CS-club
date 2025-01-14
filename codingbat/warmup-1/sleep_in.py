ec76c51c76e292dd1aac4511e5a8c0671868f086
0x7070 <raykhelson.ielon@gmail.com>
2025-01-14 13:52:06 -0500

    commit sleep_in.py

---
+++ codingbat/warmup-1/sleep_in.py
@@ -0,0 +1,6 @@
+def sleep_in(weekday, vacation):
+    return (not weekday or vacation)
+
+    # `return ()`
+    # This evaluates the condition within the parenthesis -- if the
+    #  condition is True, it returns True. Otherwise, it will return False.
